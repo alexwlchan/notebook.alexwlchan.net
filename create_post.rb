1a686f7bd8aba369104100412fdb10e8580a7461
Alex Chan <alex@alexwlchan.net>
2018-11-05 09:37:48 +0000

    Add a script for creating a new post

---
+++ create_post.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+require "fileutils"
+
+now = Time.now
+
+unless ARGV.length == 1
+  puts "Usage: create_post.rb <TITLE>"
+  exit 1
+end
+
+title = ARGV[0]
+name = title.downcase.gsub(/[^a-z]/, "-").gsub(/\-{2,}/, "-")
+path = File.join(
+  "src",
+  "_posts",
+  now.strftime("%Y"),
+  "#{now.strftime('%Y-%m-%d')}-#{name}.md"
+)
+
+doc = "---\nlayout: post\ntitle: #{title}\ndate: #{now}\ntags:\n---\n\n"
+File.open(path, 'w') { |f| f.write(doc) }
+
+puts path
