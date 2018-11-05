185e7dc3e40f688843bc0c03ca1d378bc0dcf4e0
Alex Chan <a.chan@wellcome.ac.uk>
2018-11-05 13:33:49 +0000

    Add auto-deployments with Travis (hopefully)

---
+++ travis_run.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+echo "*** Attempting to build the site"
+make build
+
+if [[ "$TRAVIS_EVENT_TYPE" == "pull_request" ]]
+then
+  echo "*** Pull request, skipping deploy"
+  exit 0
+fi
+
+echo "*** Loading Travis RSA key"
+openssl aes-256-cbc \
+  -K $encrypted_83630750896a_key \
+  -iv $encrypted_83630750896a_iv \
+  -in id_rsa.enc \
+  -out id_rsa -d
+
+echo "*** Uploading published site to Linode"
+chmod 400 id_rsa
+mv id_rsa ~/.ssh/id_rsa
+make deploy
